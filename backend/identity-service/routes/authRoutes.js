fe2980265098c8e424dbf635c686ef28855b8f9e
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-06-20 13:03:35 +0530

    feat: Add validations to the routes

--- backend/identity-service/routes/authRoutes.js
+++ backend/identity-service/routes/authRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, authUser, getUserProfile } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
+const { validateRegister, validateLogin } = require('../middleware/validationMiddleware');
 
-router.post('/register', registerUser);
-router.post('/login', authUser);
+router.post('/register', validateRegister, registerUser);
+router.post('/login', validateLogin, authUser);
 router.get('/profile', protect, getUserProfile);
 
 module.exports = router;
