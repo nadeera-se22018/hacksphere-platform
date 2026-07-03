b1797e4a63eff6bf444fea7cbcdcdb408360b1fb
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-07-03 16:03:19 +0530

    feat: Register SignalR and CORS.

--- backend/notification-leaderboard-service/Program.cs
+++ backend/notification-leaderboard-service/Program.cs
@@ -1,11 +1,22 @@
 using StackExchange.Redis;
-using notification_leaderboard_service.Services; 
+using notification_leaderboard_service.Services;
+using notification_leaderboard_service.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ClientCorsPolicy", policy => policy
+        .WithOrigins("http://localhost:3000") 
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials()); 
+});
 
 var redisConnectionString = builder.Configuration.GetConnectionString("RedisConnection")!;
 builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConnectionString));
@@ -20,8 +31,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("ClientCorsPolicy");
+
 app.UseHttpsRedirection();
 
 app.MapControllers();
+
+app.MapHub<LeaderboardHub>("/leaderboardHub");
 
 app.Run();
