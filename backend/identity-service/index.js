c48a3758d24dd2746234a6cd90307782e3e3856d
nadeera-se22018 <kasunnadeera100@gmail.com>
2026-07-06 01:32:10 +0530

    feat: Initialize the passport in the Main

--- backend/identity-service/index.js
+++ backend/identity-service/index.js
@@ -4,6 +4,9 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const passport = require('passport');
+
+require('./config/passport');
 
 dotenv.config();
 
@@ -12,7 +15,8 @@ connectDB();
 const app = express();
 
 app.use(cors()); 
-app.use(express.json()); 
+app.use(express.json());
+app.use(passport.initialize()); 
 
 app.use('/api/auth', authRoutes);
 
